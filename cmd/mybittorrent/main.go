85aa421c36a4b424062e615e34182922bd3d8537
Walter Miller <waltermil@gmail.com>
2024-01-21 10:55:09 -0500

    fetch peers

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -5,11 +5,15 @@ import (
 	"crypto/sha1"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/cmd/mybittorrent/altbencode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
+
+
 
 
 func info(torrentData []byte) {
@@ -49,6 +53,76 @@ func info(torrentData []byte) {
 	}
 }
 
+func fetchPeers(torrentData []byte) {
+	decoded, err := altbencode.Decode(string(torrentData))
+	baseMap := decoded.GetData().(map[string]altbencode.Node)
+
+	info := baseMap["info"].GetData().(map[string]altbencode.Node)
+
+	infoBencoded, err := altbencode.Encode(baseMap["info"]);
+	infoBytes := []byte(infoBencoded)
+	infoHash := sha1.Sum([]byte(infoBytes))
+
+	announceUrl := baseMap["announce"].GetData().(string)
+
+
+	peer_id := "-PC0001-123456789012"
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, announceUrl, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	q := req.URL.Query()
+
+	q.Add("info_hash", fmt.Sprintf("%s", infoHash))
+	q.Add("peer_id", peer_id)
+	q.Add("port", "6881")
+	q.Add("uploaded", "0")
+	q.Add("downloaded", "0")
+	q.Add("left", fmt.Sprint(info["length"].GetData().(int)))
+	q.Add("compact", "1")
+
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	responseBody, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// fmt.Println(string(responseBody))
+
+	responseNodes, err := altbencode.Decode(string(responseBody))
+	responseMap := responseNodes.GetData().(map[string]altbencode.Node)
+	stringPeers := responseMap["peers"].GetData().(string)
+
+	for i := 0; i < len(stringPeers); i += 6 {
+		ip := stringPeers[i:i+4]
+		port := stringPeers[i+4:i+6]
+		fmt.Printf("%d.%d.%d.%d:%d\n", ip[0], ip[1], ip[2], ip[3], int(port[0])*256+ int(port[1]))
+	}
+
+
+}
+
 func main() {
 	command := os.Args[1]
 
@@ -63,6 +137,15 @@ func main() {
 		
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
+	} else if command == "peers" {
+		torrentFilePath := os.Args[2]
+		torrentData, err := os.ReadFile(torrentFilePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fetchPeers(torrentData)
+
 	} else if command == "info" {
 		torrentFilePath := os.Args[2]
 		torrentData, err := os.ReadFile(torrentFilePath)
